d51eccd9b02c759ae1078ea3ff9f961735230871
Ammar Mukhtar <ammarmukhtar@lordevs.com>
2025-07-29 10:23:41 +0500

    first commit

--- rag_app/settings.py
+++ rag_app/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from datetime import timedelta
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'. Changes
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
