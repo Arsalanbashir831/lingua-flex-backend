24167ed0d2d1339b3d42c29bf3b2c658cb6e122b
Arsalan <arsalanbashir831@gmail.com>
2025-09-11 07:56:19 +0000

    update the settings

--- rag_app/settings.py
+++ rag_app/settings.py
@@ -67,14 +67,15 @@ if not url:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.2","localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.2","localhost", "127.0.0.1","api.shaktiwheel.in"]
 #CSRF_TRUSTED_ORIGINS = ["http://192.168.10.9:8000"]
 
 
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.10.9:3000",   # client laptop origin (React/Vite/etc)
-    
+    "https://api.shaktiwheel.in",
     "http://localhost:3000",
+    'https://linguaflex-eight.vercel.app'
 ]
 
 
